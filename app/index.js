6714259c710dec45454832069a9ff55d311ce740
pasquale <mannypasquale@me.com>
2018-06-17 12:44:09 -0700

    added more comments to index.js

--- app/index.js
+++ app/index.js
@@ -13,6 +13,13 @@ require('./index.css');
 
 
 //not valid js syntax so when ran through webpack it becomes something else
+
+//way you set what ui/specific component will look like
+// is with the render() component
+
+// what render method returns will be specifci UI for this component
+
+// component definition kinda
  class App extends React.Component { 
  	render() {
  		return (
@@ -26,8 +33,14 @@ require('./index.css');
 
  //take component and render to react-dom
 
+ // how we use a component
+ // you need to first say which component you want to render!!
+ // then the 2nd argument is the DOM node where you want to render the component
+
+ // in this case App is the component we made above and the element id we are going to assign it is 'app'
+ // this means in the index.html there will be a div with the id of 'app'
  ReactDOM.render(
- 	<App />, 
- 	document.getElementById('app')
+ 	<App />, // <-- this is JSX too tells you that you actually want to use the componentn 
+ 	document.getElementById('app')  //once we have our template we gonna have div in their with the id of app
  );
 
